9be853333ee8b0b4d4d5019d02192a1219d5aa59
Jack Fitton <93110524+jackfitton112@users.noreply.github.com>
2023-07-20 10:23:26 +0100

    comments

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ activity = discord.Activity(name='# !xkcd <post> or just !xkcd', type=discord.Ac
 # Get the token from the .env file
 TOKEN = os.getenv("DISCORD_TOKEN") # Dont forget to add your token to the .env file
 
+# Sends a message to the channel with the help menu
 async def help(channel: discord.TextChannel) -> None:
 
     message = '''
@@ -37,6 +38,7 @@ async def help(channel: discord.TextChannel) -> None:
 
     await channel.send(message)
 
+# Sends a message to the channel with the error message
 async def error(channel: discord.TextChannel, error) -> None:
 
     #list of funny error messages
@@ -58,6 +60,7 @@ async def error(channel: discord.TextChannel, error) -> None:
     #log the error
     print(error)
 
+# Sends a message to the channel with the comic not found message 
 async def commic_out_of_range(channel: discord.TextChannel) -> None:
 
     comic_not_found_messages = [
@@ -75,6 +78,7 @@ async def commic_out_of_range(channel: discord.TextChannel) -> None:
 
     await channel.send(comic_not_found_messages[randint(0, len(comic_not_found_messages) - 1)])
 
+#handles the sending and formatting of the post
 async def send_post(channel: discord.TextChannel, post: tuple) -> None:
 
     post_id = post[1]
@@ -91,6 +95,7 @@ async def send_post(channel: discord.TextChannel, post: tuple) -> None:
 
     await channel.send(embed=embed)
 
+#handles the sending and formatting of the explination
 async def send_explination(channel: discord.TextChannel, post: tuple) -> None:
 
     try:
@@ -102,6 +107,7 @@ async def send_explination(channel: discord.TextChannel, post: tuple) -> None:
     for i in range(0, len(explination), 1999):
         await channel.send(explination[i:i+1999])
 
+#debug function to test the api
 async def debug(channel: discord.TextChannel) -> None:
     
     #send latest post
@@ -132,15 +138,6 @@ async def debug(channel: discord.TextChannel) -> None:
 
     
 
-
-
-
-
-    
-
-
-
-
 @client.event
 async def on_ready():
     # This is called when the bot has connected to discord
@@ -154,32 +151,35 @@ async def on_message(message):
     if message.author == client.user:
         return # Ignore messages from the bot
 
+    #checks if the bot is being called
     if message.content.startswith('!xkcd'):
 
 
+        #get the command or set it to help if no command is given
         if len(message.content.split()) > 1:
             command = message.content.split()[1]
 
         else:
             command = "help"
 
-        print(f"User {message.author} sent command: {command}")
+        #DEBUG
+        #print(f"User {message.author} sent command: {command}")
 
         if command == "help":
             await help(message.channel)
 
         elif command == "latest":
-            data = api.get_latest_post()
+            data = api.get_latest_post() # get the latest post from the api
             await send_post(message.channel, data)
 
         elif command == "random":
-            data = api.get_random_post()
+            data = api.get_random_post() # get a random post from the api
             await send_post(message.channel, data)
 
         elif command == "explain":
-            post_id = message.content.split()[2]
-            data = api.get_post_by_id(post_id)
-            await send_explination(message.channel, data)
+            post_id = message.content.split()[2] # get the post id from the message
+            data = api.get_post_by_id(post_id)  # get the post from the api
+            await send_explination(message.channel, data) # send the explination
 
         elif command == "debug":
             await debug(message.channel)
@@ -190,15 +190,17 @@ async def on_message(message):
             search_term = message.content.replace("!xkcd search ", "")
             dataset = api.search_posts(search_term)
 
-            if dataset:
-                await message.reply(f"Found {len(dataset)} results for '{search_term}', would you like to send them? (y/n)")
+            if dataset: # if the search returns results
+                await message.reply(f"Found {len(dataset)} results for '{search_term}', would you like to send them? (y/n)") # ask the user if they want to send the results
 
-                def check(m):
+                def check(m): # check if the message is from the same user and if it is yes or no
                     return m.author == message.author and m.content.lower() in ["y", "yes", "n", "no"]
 
                 try:
+                    # wait for a response for 30 seconds
                     msg = await client.wait_for('message', check=check, timeout=30)
 
+                    # if the user says yes send the posts, else cancel the search
                     if msg.content.lower() in ["y", "yes"]:
                         for data in dataset:
                             post = api.get_post_by_id(data[0])
@@ -206,21 +208,24 @@ async def on_message(message):
                     else:
                         await message.reply("Search cancelled")
 
+                #add a catch for timeout
                 except asyncio.TimeoutError:
                     await message.reply("Search timed out. Try again if you want to proceed.")
 
             else:
+                # if the search returns no results, send a message to the channel
                 await commic_out_of_range(message.channel)
     
 
-        elif command.isdigit():
+        elif command.isdigit(): # if the command is a number, get the post by id
             data = api.get_post_by_id(command)
-            if data:
+            if data: # if the post exists, send it
                 await send_post(message.channel, data)
-            else:
+            else: # if the post does not exist, send a message to the channel
                 await commic_out_of_range(message.channel)
 
         else:
+            #if unhandled command, send help            
             await help(message.channel)
 
 
@@ -230,6 +235,7 @@ async def on_message(message):
 if type(TOKEN) == str:
     client.run(TOKEN)
 else:
+    #stops the bot if no token is found
     exit("No token found - Please add your token to the .env file")
     
 
